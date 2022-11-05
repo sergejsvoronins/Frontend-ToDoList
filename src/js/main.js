493a38219ed9de707f7ee3b65fdb4b8b2cfc43fe
sergejsvoronins <svoronins@gmail.com>
2022-11-05 09:44:25 +0100

    created btn container for all btn to make more nice design

--- src/js/main.js
+++ src/js/main.js
@@ -1,78 +1,95 @@
 class ToDoObject {
     constructor(toDo){
         this.toDo = toDo;
+        this.done = "klar";
     }
 }
-
 //All creations
 let mainContainer = document.createElement("div");
 let toDoHeader = document.createElement("h2");
 let userInputLabelEl = document.createElement("label");
 let userInputEl = document.createElement("input");
+let btnContainer = document.createElement("div");
 let createToDoBtn = document.createElement("button");
 let createNewToDoBtn = document.createElement("button");
-let toDoListUlTag = document.createElement("ol");
-
-
-
+let toDoListUlTag = document.createElement("ul");
 //All appends
 document.body.append(mainContainer);
 mainContainer.appendChild(toDoHeader);
 mainContainer.appendChild(userInputLabelEl);
 mainContainer.appendChild(userInputEl);
-mainContainer.appendChild(createToDoBtn);
-mainContainer.appendChild(createNewToDoBtn);
+mainContainer.appendChild(btnContainer);
+btnContainer.appendChild(createToDoBtn)
+btnContainer.appendChild(createNewToDoBtn)
 mainContainer.appendChild(toDoListUlTag);
-
-
 //Create classes/ID
-mainContainer.className = "container"
+mainContainer.className = "container";
 userInputLabelEl.className = "container__inputLabel";
 userInputEl.className = "container__inputField"
-createToDoBtn.className = "container__btn";
-createToDoBtn.className = "container__btn";
+btnContainer.className = "container__btnContainer";
+createToDoBtn.className = "container__btnContainer__btn";
+createNewToDoBtn.className = "container__btnContainer__btn";
 userInputEl.id = "userInput";
-
-
 //Create attributes
 userInputLabelEl.setAttribute("for", "userInput");
 userInputEl.setAttribute("type", "text");
 userInputEl.setAttribute("placeholder", "Jag ska göra..");
-
 createToDoBtn.setAttribute("type", "button");
+createNewToDoBtn.setAttribute("type", "button");
 //Set innerHTML
-toDoHeader.innerHTML = "ToDo List"
-userInputLabelEl.innerHTML = "Vad ska du göra?"
-createToDoBtn.innerHTML = "Skapa uppgift";
-createNewToDoBtn.innerHTML = "Skapa en ny lista";
+toDoHeader.innerHTML = "ToDo Lista"
+userInputLabelEl.innerHTML = "Skriv dagens uppgift"
+createToDoBtn.innerHTML = "Lägg till";
+createNewToDoBtn.innerHTML = "Ny lista";
+
 
 
 
 let toDoList =[];
 
 
-createToDoBtn.addEventListener("click", addToDo);
-createNewToDoBtn.addEventListener("click", addNewToDo);
 
-
-
-function addToDo(){
+createToDoBtn.addEventListener("click", ()=>{
     toDoListUlTag.innerHTML ="";
     let newToDo = new ToDoObject(userInputEl.value);
     toDoList.push(newToDo);
     for(let i=0; i< toDoList.length; i++){
+        let counter = 0;
         let newTask = document.createElement("li");
         toDoListUlTag.appendChild(newTask);
         newTask.innerHTML = toDoList[i].toDo;
-        console.log(toDoList);
+        newTask.addEventListener("click",()=>{
+            if(counter%2===0){
+                newTask.innerHTML = toDoList[i].done;
+                counter++;
+            }
+            else {
+                newTask.innerHTML = toDoList[i].toDo;
+                counter++;
+            }
+            
+        })
+        newTask.addEventListener("mouseenter", ()=>{
+            newTask.style.fontWeight = "700";
+        })
+        newTask.addEventListener("mouseleave", ()=>{
+            newTask.style.fontWeight = "400";
+        })
     }
-    
-}
-
-
-function addNewToDo(){
+});
+createNewToDoBtn.addEventListener("click", ()=>{
     toDoListUlTag.innerHTML ="";
-}
+    completedTasksUlTag.innerHTML ="";
+    toDoList = [];
+});
+
+
+
+
+
+ 
+
+
 
 
 
